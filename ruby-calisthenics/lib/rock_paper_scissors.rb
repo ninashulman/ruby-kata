5ab5ffc3871613507e838a6d0d78a4b7b6ef6a3c
ninashulman <saasbook@saasbook.(none)>
2014-11-21 19:55:17 -0400

    hw 1-3: RPS

--- ruby-calisthenics/lib/rock_paper_scissors.rb
+++ ruby-calisthenics/lib/rock_paper_scissors.rb
@@ -1,14 +1,34 @@
 class RockPaperScissors
 
   # Exceptions this class can raise:
-  class NoSuchStrategyError < StandardError ; end
+  class NoSuchStrategyError < StandardError
+# RockPaperScissors::NoSuchStrategyError with "Strategy must be one of R,P,S"
+  end
 
   def self.winner(player1, player2)
-    # YOUR CODE HERE
+    choices = /[RPS]/
+    p1 = player1[1]
+    p2 = player2[1]
+    print p1
+    print p2
+    raise RockPaperScissors::NoSuchStrategyError,"Strategy must be one of R,P,S" unless (p1 =~ choices) && (p2 =~ choices)
+
+    if (p1 == p2) || (p1 == 'P' && p2 == 'R') || (p1 == 'R' && p2 == 'S') || (p1 == 'S' && p2 == 'P')
+	return player1
+    else
+	return player2
+    end
   end
 
   def self.tournament_winner(tournament)
-    # YOUR CODE HERE
+    if (tournament[0][0].is_a? String)
+	self.winner(tournament[0], tournament[1])
+    else
+	self.tournament_winner(
+		[self.tournament_winner(tournament[0]),
+		 self.tournament_winner(tournament[1])]
+		)
+    end
   end
 
 end
