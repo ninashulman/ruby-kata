ec0a9ee974cd838b2b3dfc62a5618f88f34cf7d5
ninashulman <saasbook@saasbook.(none)>
2014-11-22 03:31:10 -0400

    hw 1-4 Ruby Meraprogramming

--- ruby-calisthenics/lib/attr_accessor_with_history.rb
+++ ruby-calisthenics/lib/attr_accessor_with_history.rb
@@ -4,7 +4,14 @@ class Class
     attr_reader attr_name # create the attribute's getter
     attr_reader attr_name+"_history" # create bar_history getter
     class_eval %Q{
-      # YOUR CODE HERE
+	def #{attr_name}=(name)
+	   @#{attr_name} = name		   	               
+           if  !@#{attr_name}_history 
+               @#{attr_name}_history = []
+               @#{attr_name}_history << nil
+               @#{attr_name}_history << name
+           end                                            
+        end
     }
   end
 end
